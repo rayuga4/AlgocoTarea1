54d0bc817f6c35c740f4f4ea231488bd03a0d35b
rayuga4 <40147972+rayuga4@users.noreply.github.com>
2024-09-10 20:05:58 -0300

    Update algoritmoCuadratico.py

--- algoritmoCuadratico.py
+++ algoritmoCuadratico.py
@@ -13,11 +13,13 @@ start = time.time()
 for x in range(len(list)):
     flag=False
     for y in range(len(list)-x-1):
+# si el elemento actual es mayor al siguiente, se intercambian
         if list[y]>list[y+1]:
             aux = list[y+1]
             list[y+1] = list[y]
             list[y] = aux
             flag = True
+# si no se realiza ningun swap en una iteracion, la lista esta ordenada y se sale del ciclo
     if not flag:
         break
 
